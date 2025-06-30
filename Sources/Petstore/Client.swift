d6e233f6016249cddc599fa6b6620b720558bf2a
Anar Kafkas <anarkafkas@gmail.com>
2025-06-30 19:26:30 +0300

    Add docs for `PetstoreClient`

--- Sources/Petstore/Client.swift
+++ Sources/Petstore/Client.swift
@@ -1,3 +1,18 @@
+/// Use this class to access the different functions within the SDK. You can instantiate any number of clients with different configuration that will propagate to these functions.
+///
+/// - Parameters:
+///   - baseURL: The base URL to use for requests from the client.
+///   - authConfig: The authentication configuration to use for requests from the client. Defaults to `NoAuth()`.
+///
+/// # Examples
+/// ```swift
+/// import Petstore
+///
+/// let client = PetstoreClient(
+///     baseURL: "https://petstore3.swagger.io/api/v3",
+///     authConfig: ApiKeyAuth(apiKey: "YOUR_API_KEY")
+/// )
+/// ```
 public struct PetstoreClient: Sendable {
     public let pet: PetClient
     public let store: StoreClient
